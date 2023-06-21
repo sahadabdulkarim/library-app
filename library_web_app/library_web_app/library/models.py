999ddf2db2be7b12af8641691e69d664e00d0eac
PP Sahad Abdul Karim <ppsahadabdulkarim@gmail.com>
2023-06-21 10:48:02 +0530

    just the outline created

--- library_web_app/library_web_app/library/models.py
+++ library_web_app/library_web_app/library/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    publication_date = models.DateField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    is_available = models.BooleanField(default=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+    book_code = models.CharField(max_length=20)
+    category = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
